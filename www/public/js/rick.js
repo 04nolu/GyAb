757879848f8ea399cf632c72cf447a8a798ad61e
04nolu <04nolu@skola.engelholm.se>
2023-05-21 16:42:24 +0200

    Vi säger att jag är klar med footer

---
+++ www/public/js/rick.js
@@ -0,0 +1,6 @@
+document.querySelector("h4").addEventListener("click", Rick)
+
+function Rick() {
+    console.log("Hej")
+    window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+}
